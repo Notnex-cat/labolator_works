a1841250c748b182819be095ef2b4292c12ec0fa
Notnex-cat <80640249+Notnex-cat@users.noreply.github.com>
2023-01-10 00:19:03 +0300

    Add files via upload

---
+++ ООП C#/Laba1/Laba1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите х в градусах ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y в градусах");
+double y = Convert.ToDouble(Console.ReadLine());
+x = x * (Math.PI / 180);
+y = y * (Math.PI / 180);
+double res = Math.Pow(Math.Cos(x), 4) + Math.Pow(Math.Sin(y), 2) + 0.25 * Math.Pow(Math.Sin(2 * x), 2) - 1;
+Console.WriteLine(res);
+
+Console.WriteLine("Введите х в градусах ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y в градусах ");
+double y = Convert.ToDouble(Console.ReadLine());
+x = x * (Math.PI / 180);
+y = y * (Math.PI / 180);
+double res = Math.Sin(y - x) * Math.Sin(y - x);
+Console.WriteLine(res);
