9de5a4a060bbe4a74b0434bb1a767803fca44d6a
Notnex-cat <80640249+Notnex-cat@users.noreply.github.com>
2023-01-09 23:39:55 +0300

    Add files via upload

---
+++ Математические модели и методы/3.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("__________________________________________________");
+Console.WriteLine("| №  |          x1          |          x2          |");
+Console.WriteLine("--------------------------------------------------");
+double x = -0.5, y = 1.5, q1, q2, e = 1e-6, x_0 = 0, y_0 = 0;
+q1 = Math.Abs(fi1_dx(x, y)) + Math.Abs(fi1_dy(x, y));
+q2 = Math.Abs(fi2_dx(x, y)) + Math.Abs(fi2_dy(x, y));
+int i = 0;
+while(Math.Abs(x-x_0) > e && Math.Abs(y-y_0) > e)
+{
+    x_0 = x;
+    y_0 = y;
+    x = fi1(x_0, y_0);
+    y = fi2(x_0, y_0);
+    i++;
+    Console.WriteLine("| {0,-2} | {1,-20} | {2,-20} | ", i, x, y);
+}
+Console.WriteLine("__________________________________________________");
+
+double fi1(double x, double y)//x1(x)
+{
+    return 0.6-Math.Sin(y);
+}
+
+double fi2(double x, double y)//x2(y)
+{
+    return -Math.Cos(y+x)/2;
+}
+
+double fi1_dx(double x, double y)
+{
+    return 0;
+}
+
+double fi1_dy(double x, double y)
+{
+    return -Math.Cos(y);
+}
+
+double fi2_dx(double x, double y)
+{
+    return Math.Sin(y+x)/2;
+}
+
+double fi2_dy(double x, double y)
+{
+    return Math.Sin(y + x) / 2;
+}
---
+++ Математические модели и методы/5.cs
@@ -0,0 +1,111 @@
+﻿//трапеции
+double a = 0, b = 1, n = 400;
+double x, h, s, y;
+h = (b - a) / (n); //шаг
+s = 0;
+for (x = a + h / 2; x < b; x += h)
+{
+    y = x * Math.Cos(2 * x);
+    s += y;
+}
+s *= h;
+Console.WriteLine(s);
+//Считаем при h2
+double h2 = 2 * (b - a) / n; //шаг h2
+double s1 = 0;
+for (x = a + h2 / 2; x < b; x += h2)
+{
+    y = x * Math.Cos(2 * x);
+    s1 += y * h2;
+}
+Console.WriteLine(s1);
+
+Console.WriteLine(Math.Abs(s - s1) / 3.0);//правило  рунге
+
+//симпсон
+double b2, b1, n = 400;
+static double f(double x)
+{
+    return x * Math.Cos(2 * x);
+}
+
+double x, a, b, h, h2, s, s1;
+a = 0;
+b = 1;
+h = (b - a) / n;
+s = 0;
+x = h;
+while (x < b)
+{
+    s += 4 * f(x);
+    x += h;
+    s += 2 * f(x);
+    x += h;
+}
+b1 = h / 3 * (s + f(a) - f(b));// занесём 2 внутрь скобки, поэтому умножаем на 2 и 4
+Console.WriteLine(" Интеграл(Метод Симпсона) h = {0}", b1);
+
+h2 = 2 * (b - a) / n;
+s1 = 0;
+x = h2;
+while (x < b)
+{
+    s1 += 4 * f(x);
+    x += h2;
+    s1 += 2 * f(x);
+    x += h2;
+}
+b2 = h2 / 3 * (s1 + f(a) - f(b));
+Console.WriteLine(" Интеграл(Метод Симпсона) 2h = {0}", b2);
+Console.WriteLine("Метод Рунге= " + Math.Abs(b2 - b1) / 3.0);//runge
+
+
+//Квадратурная формула прямоугольников
+double f(double x)
+{
+    return x * Math.Cos(2 * x);
+}
+
+double a = 0, b = 1, n = 400, I = 0;
+
+double h = (b - a) / n;
+for (double x = a; x <= b; x += h)
+{
+    I += f(x - h / 2);
+}
+I *= h;
+
+Console.WriteLine(I);
+
+
+
+//Левых прямоуг
+double f(double x)
+{
+    return x * Math.Cos(2 * x);
+}
+double a = 0, b = 1, n = 400, I = 0;
+
+double h = (b - a) / n;
+for (int i = 0; i <= n - 1; i++)
+{
+    I += f(a + i * h);
+}
+I *= h;
+Console.WriteLine(I);
+
+
+//Правых
+double f(double x)
+{
+    return x * Math.Cos(2 * x);
+}
+double a = 0, b = 1, n = 400, I = 0;
+
+double h = (b - a) / n;
+for (int i = 1; i <= n; i++)
+{
+    I += f(a + i * h);
+}
+I *= h;
+Console.WriteLine(I);
