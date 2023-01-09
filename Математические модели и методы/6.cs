492fafd183a7a7e9968ff3fd63db937b999625da
Notnex-cat <80640249+Notnex-cat@users.noreply.github.com>
2023-01-09 23:44:44 +0300

    Add files via upload

---
+++ Математические модели и методы/6.cs
@@ -0,0 +1,64 @@
+﻿double x = 1, y = 2, a = 1, b = 1.6, h = 0.1;
+rungeKutta(x, y, a, b, h);
+euler(x, y, h);
+real();
+
+void rungeKutta(double x, double y, double a, double b, double h)
+{
+    double f1, f2, f3, f4;
+    int n = (int)((b - a) / h);
+
+    Console.WriteLine("  Метод Рунге–Кутта (IV)");
+    Console.WriteLine("___________________________");
+    Console.WriteLine("|  x  |          y         |");
+    Console.WriteLine("----------------------------");
+
+    for (int i = 0; i < n; i++)
+    {
+        f1 = h * (f(x, y));
+        f2 = h * (f(x + 0.5 * h, y + 0.5 * f1));
+        f3 = h * (f(x + 0.5 * h, y + 0.5 * f2));
+        f4 = h * (f(x + h, y + f3));
+
+        y += (1.0 / 6.0) * (f1 + 2 * f2 + 2 * f3 + f4);
+        x += h;
+
+        Console.WriteLine("| {0,-3} | {1,-18} |", (float)x, y);
+    }
+    Console.WriteLine("----------------------------\n");
+}
+
+void euler(double x, double y, double h)
+{
+    Console.WriteLine("      Метод Эйлера");
+    Console.WriteLine("___________________________");
+    Console.WriteLine("|  x  |          y         |");
+    Console.WriteLine("----------------------------");
+    for (int i = 0; i < 6; i++)
+    {
+        y = y + h * f(x, y);
+        x = x + h;
+        Console.WriteLine("| {0,-3} | {1,-18} |", (float)x, y);
+    }
+    Console.WriteLine("----------------------------\n");
+}
+
+static double f(double x, double y)
+{
+    return Math.Pow(2.71, 2*x) * (2 + 3 * Math.Cos(x)) / (2 * y) - (3 * y * Math.Cos(x)) / 2;
+}
+void real()
+{
+    double x = 0;
+    Console.WriteLine("      Реальное решение");
+    Console.WriteLine("___________________________");
+    Console.WriteLine("|  x  |          y         |");
+    Console.WriteLine("----------------------------");
+    for (int i = 0; i < 7; i++)
+    {
+        x = 1 + 0.1 * i;
+        y = Math.Sqrt(Math.Pow(2.71, 2 * x) - (-4 + 2.71 * 2.71) * Math.Pow(2.71, 3 * (-Math.Sin(x)+Math.Sin(1))));
+        Console.WriteLine("| {0,-3} | {1,-18} |", (float)x, y);
+    }
+    Console.WriteLine("----------------------------");
+}
---
+++ Математические модели и методы/7.cs
@@ -0,0 +1,76 @@
+﻿//дихотомия
+double e = 1e-6, h = 1e-2;//погрешность вычисления
+double a = 2, b = 30;//интервал [2, 30]
+int k = 0;//кол-во итераций
+Console.WriteLine("_______________________________________");
+Console.WriteLine("| Кол-во итераций |      Корень x      |");
+Console.WriteLine("---------------------------------------");
+while (b - a >= e)
+{
+    Console.WriteLine("|        {0, -9}| {1, -18} |", k, (a + b) / 2);//выводим таблицу
+    double c = (a + b) / 2;
+    if (f(c) - f(c + h) > 0)//> min, < max
+        a = c;
+    else
+        b = c;
+    k++;
+}
+Console.WriteLine("_______________________________________");
+
+static double f(double x)
+{
+    return (double)(40 - 3 * x / 4 + x * x / 30 + 70 * x);
+}
+
+
+//метод координатного спуска
+double d1(double x)
+{
+    double e = 1e-4, h = 0.6;//погрешность вычисления
+    double a = -50, b = 50;//интервал
+    while (b - a >= e)
+    {
+        double c = (a + b) / 2;
+        if (f(x, c) - f(x, c + h) < 0)//> min, < max
+            a = c;
+        else
+            b = c;
+    }
+    if (a < b) { return a; }
+    else { return b; }
+}
+
+double d2(double x)
+{
+    double e = 1e-4, h = 0.6;//погрешность вычисления
+    double a = -50, b = 50;//интервал 
+    while (b - a >= e)
+    {
+        double c = (a + b) / 2;
+        if (f(c, x) - f(c + h, x) < 0)//> min, < max
+            a = c;
+        else
+            b = c;
+
+    }
+    if (a < b) { return a; }
+    else { return b; }
+}
+
+double f(double x1, double x2)
+{
+    return 1 / ((x1 + 1) * (x1 + 1) + x2 * x2);
+}
+
+
+double eps = 1e-4;
+double x1 = 2, x2 = 2.8, xt1 = 0, xt2 = 0;
+do
+{
+    xt1 = x1;
+    xt2 = x2;
+    x2 = d1(x1);
+    x1 = d2(x2);
+
+} while (Math.Abs(xt1 - x1) > eps && Math.Abs(xt2 - x2) > eps);
+Console.WriteLine("x1= " + x1 + " " + "x2= " + x2);
